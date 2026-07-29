d96a055e7e9bf9d5e863718081065a254cae8996
Ahmed Rangel <ahmedrangel@outlook.com>
2026-07-28 19:52:00 -0500

    feat!: update project name and domain

--- shared/utils/site.ts
+++ shared/utils/site.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  name: "streaming | JimTracker",
-  domain: "streaming.jimtracker.com",
+  name: "watch | JimTracker",
+  domain: "watch.jimtracker.com",
   flvURL: import.meta.dev ? "http://127.0.0.1:8080/live/jimrsng.flv" : "https://flv.jimtracker.com/live/jimrsng.flv",
-  host: import.meta.dev ? "http://localhost:5173" : "https://streaming.jimtracker.com",
+  host: import.meta.dev ? "http://localhost:5173" : "https://watch.jimtracker.com",
   github: {
     author: "JimRsng",
-    repo: "https://github.com/JimRsng/streaming"
+    repo: "https://github.com/JimRsng/watch"
   }
 };
