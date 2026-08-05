715512637966be2e97040e0dceab36fba275caec
Ahmed Rangel <ahmedrangel@outlook.com>
2026-08-05 10:33:54 -0500

    fix: mobile chat toggler and viewer count styling

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -145,9 +145,9 @@ onMounted(async () => {
               <div ref="chat-frame" class="relative h-full">
                 <ChatFrame :src="embed.kick.chat" :class="{ hidden: chatToggle === 'twitch' }" />
                 <ChatFrame :src="embed.twitch.chat" :class="{ hidden: chatToggle === 'kick' }" />
-                <div v-if="isMobile" class="flex justify-start w-full gap-2 absolute z-1 top-0">
+                <div v-if="isMobile" class="flex justify-start w-full gap-2 absolute z-1 top-0 py-2.5 px-2">
+                  <ChatToggler v-model="chatToggle" class="text-white" />
                   <ChatViewerCount v-if="!playerErrored && liveData.isLive" :count="liveData.viewerCount" />
-                  <ChatToggler v-model="chatToggle" class="p-2 text-white" />
                 </div>
               </div>
             </Pane>
