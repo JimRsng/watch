3b0c4b625118d8f489cfa80f901cd532ea77a4ce
Ahmed Rangel <ahmedrangel@outlook.com>
2026-08-05 02:46:25 -0500

    fix: add error handling to WebSocket connection failure

--- app/utils/ws.client.ts
+++ app/utils/ws.client.ts
@@ -7,32 +7,41 @@ export const createWebsocket = async ({
   liveData: Ref<{ viewerCount: number, isLive: boolean, sessionId?: string }>;
   uuid: Ref<string | null>;
 }) => {
-  ws.value = new WebSocket(SITE.wsURL);
-  const connect = async () => {
-    await new Promise<void>((resolve) => {
-      ws.value?.addEventListener("open", () => {
-        console.info("[jimtracker] Connected to WebSocket server.");
-        resolve();
+  try {
+    ws.value = new WebSocket(SITE.wsURL);
+    const connect = async () => {
+      await new Promise<void>((resolve, reject) => {
+        ws.value?.addEventListener("open", () => {
+          console.info("[jimtracker] Connected to WebSocket server.");
+          resolve();
+        });
+        ws.value?.addEventListener("error", reject);
+        ws.value?.addEventListener("close", reject);
       });
+    };
+    await connect();
+    ws.value.addEventListener("message", (message) => {
+      const { type, viewerCount, isLive } = JSON.parse(message.data?.toString() || "{}") as { type: "liveInfo", viewerCount: number, isLive: boolean };
+      if (type === "liveInfo") {
+        liveData.value.viewerCount = viewerCount;
+        liveData.value.isLive = isLive;
+        ws.value?.send(JSON.stringify({ type: "heartbeat", uuid: uuid.value }));
+      }
     });
-  };
-  await connect();
-  ws.value.addEventListener("message", (message) => {
-    const { type, viewerCount, isLive } = JSON.parse(message.data?.toString() || "{}") as { type: "liveInfo", viewerCount: number, isLive: boolean };
-    if (type === "liveInfo") {
-      liveData.value.viewerCount = viewerCount;
-      liveData.value.isLive = isLive;
-      ws.value?.send(JSON.stringify({ type: "heartbeat", uuid: uuid.value }));
-    }
-  });
-  ws.value.addEventListener("error", (error) => {
-    console.info("[jimtracker] WebSocket error:", error);
-    liveData.value.isLive = false;
-    ws.value?.close();
-  });
-  ws.value.addEventListener("close", () => {
-    console.info("[jimtracker] Disconnected from WebSocket server.");
+    ws.value.addEventListener("error", (error) => {
+      console.info("[jimtracker] WebSocket error:", error);
+      liveData.value.isLive = false;
+      ws.value?.close();
+    });
+    ws.value.addEventListener("close", () => {
+      console.info("[jimtracker] Disconnected from WebSocket server.");
+      liveData.value.isLive = false;
+      ws.value = null;
+    });
+  }
+  catch (error) {
+    console.info("[jimtracker] Failed to connect to WebSocket server:", error);
     liveData.value.isLive = false;
     ws.value = null;
-  });
+  }
 };
