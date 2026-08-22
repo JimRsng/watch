5d0b8bbef155a88a3a96686b4d13317c3371fe25
Yizack Rangel <yizackr@gmail.com>
2026-08-22 03:41:53 +0200

    fix(composable): make live-data client-only

--- app/composables/live-data.ts
+++ app/composables/live-data.ts
@@ -2,6 +2,7 @@ export const useLiveData = async () => {
   return useFetch<LiveData>("/live", {
     baseURL: SITE.serverURL,
     deep: true,
+    server: false,
     default: () => ({
       isLive: false,
       viewerCount: 0,
